30cc33b5301a24ff03f4b3a17414a1af5d672e8a
Carl <carlbrainegebe@gmail.com>
2021-05-28 15:18:30 -0500

    commit tree a81504358a5012e362cc2edc4f72ee23b879a6ad

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then((json) => renderBooks(json));
+
   
 }
 
@@ -10,7 +14,28 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+
+  const bookFive = books[4];
+  console.log("title of book five is: ", bookFive["name"])
+
+  //use .map to store the value of number of pages into a new array
+  const pageNumSummary = books.map(   (book) => { console.log(`number of pages in ${book["name"]}`, book["numberOfPages"]); }  );
+
 }
+
+//show the fifth book in the series
+// function fifthBook(books) {
+// //
+// const bookFive = books[4];
+// console.log("title of book five is: ", bookFive["name"])
+// }
+
+// function totalPages(books) {
+//   //use .map to store the value of number of pages into a new array
+//   books.map(   (book) => { cosole.log("\n", book["numberOfPages"]); }  );
+// }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
